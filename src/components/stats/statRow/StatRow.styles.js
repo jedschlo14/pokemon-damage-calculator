00d09e3ce62e44aee0a0d584f3342f9910d73237
jedschlo14 <jedschloesser@gmail.com>
2022-07-17 13:43:45 -0400

    Reformatted stats

--- src/components/stats/statRow/StatRow.styles.js
+++ src/components/stats/statRow/StatRow.styles.js
@@ -2,5 +2,6 @@ import styled from "@emotion/styled";
 import { CenteredFlexbox } from "assets/styles/Common.styles";
 
 export const StatRowTitle = styled(CenteredFlexbox)`
+    justify-content: end;
     font-weight: 800;
 `;
